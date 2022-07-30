f6720f8d022d89cb08d9e55a1589baa7cbe39975
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-07-30 14:21:29 -0300

    feat: implement DocumentBuilder and SwaggerModule

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Via Varejo')
+    .setDescription(
+      'Este projeto foi desenvolvido no entuíto de resolver o desafio para vaga de back-end da Via Varejo.',
+    )
+    .setVersion('1.0.0')
+    .addTag('checkout')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3333);
 }
 bootstrap();
